ae52287661041b2a78f2282dbda242b66ff0a23c
Adil Yıldız <yildizadil81@gmail.com>
2022-09-17 22:35:40 +0300

    6

---
+++ team-work6/vowels/app.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("input");
+
+console.log(input);
+console.log(input.value);
+
+const btn = document.querySelector("button");
+console.log(btn);
+// btn.addEventListener("click");
+console.log(input.value);
